9629a5d28818a87ef18a32787aee24fd898afac5
Daniel Chen <dychen@caltech.edu>
2012-09-03 13:29:57 -0400

    Trying to get dbshell to work on Heroku

--- testdjango/production_settings.py
+++ testdjango/production_settings.py
@@ -2,3 +2,4 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 BROKER_URL = 'amqp://app7168026_heroku.com:F_AF6puAqTqTS9Ezj_j3LF_NOwvB6Gxb@tiger.cloudamqp.com/app7168026_heroku.com'
+DATABASE_URL= 'postgres://sqguvtgbcxghvw:GroOvsQuE8TMB_hZfuZMo7Dsn5@ec2-23-21-216-174.compute-1.amazonaws.com:5432/d4rens7jb901f0'
